2c1c3295426b02a4597ad4adf1f20fb4904e7d65
Martin Cruz <slayer.dmx@gmail.com>
2014-11-30 11:57:16 -0500

    Animation and render canvas in JPanel

--- src/app/views/Main.java
+++ src/app/views/Main.java
@@ -15,24 +15,7 @@ public class Main {
         
         AplicationFrame aplication = new AplicationFrame();
         aplication.setLocationRelativeTo(null);
-        aplication.setVisible(true);
-        
-        /*
-        GLProfile.initSingleton();
-        final GLProfile profile = GLProfile.get(GLProfile.GL2);
-        GLCapabilities capabilities = new GLCapabilities(profile);
-        // The canvas 
-        final GLCanvas glcanvas = new GLCanvas(capabilities);
-        CubeTexture r = new CubeTexture();
-        glcanvas.addGLEventListener(r);
-        glcanvas.setSize(400, 400);
-        
-        final JFrame frame = new JFrame(" Textured Cube");
-        frame.getContentPane().add(glcanvas);
-        frame.setSize(frame.getContentPane().getPreferredSize());
-        frame.setVisible(true);
-        final FPSAnimator animator = new FPSAnimator(glcanvas, 300, true);
-        animator.start();
-                */
+        aplication.setVisible(true);      
+       
     }
 }
