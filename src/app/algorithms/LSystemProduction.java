f9ed9545e4e4a3befd4a0860502ca2ff8985dfec
Martin Cruz <slayer.dmx@gmail.com>
2014-12-19 19:55:35 -0500

    Correcion de generar palabra LSystem

--- src/app/algorithms/LSystemProduction.java
+++ src/app/algorithms/LSystemProduction.java
@@ -1,14 +1,13 @@
-
 package app.algorithms;
 
 import java.util.Vector;
 
 public class LSystemProduction extends Thread {
-    
+
     private String axiom;
     private Vector<String> rules;
     private Vector<String> productions;
-    private int numProductions;   
+    private int numProductions;
 
     public LSystemProduction(String axiom, Vector<String> rules, int numProductions) {
         this.axiom = axiom;
@@ -19,42 +18,30 @@ public class LSystemProduction extends Thread {
 
     @Override
     public void run() {
- 
-        String word = axiom;    
-        
-        for (int i = 0; i < numProductions; i++) {
-            int cont = 0;
-            
-            while ( cont < word.length() ) {
-                
-                if ( word.charAt(cont) == 'F' ) {
-                    
-                    // Generating random number between 0 and 1
-                    float randProd = (float) Math.random();
-                    
-                    if (randProd > 0.0f && randProd <= 0.33f) {
-                        word = word.replaceFirst("F", rules.get(0));
-                        cont += rules.get(0).length();
-                    } 
-                    else if (randProd > 0.33f && randProd <= 0.66f) {
-                        word = word.replaceFirst("F", rules.get(1));
-                        cont += rules.get(1).length();
-                    }
-                    else if (randProd > 0.66f && randProd <= 1.0f) {
-                        word = word.replaceFirst("F", rules.get(2));
-                        cont += rules.get(2).length();
-                    }
-                }
-                else {
-                    cont ++;
-                }
+
+        String word = axiom;
+
+        for (int i = 0; i <= numProductions; i++) {
+
+            // Generating random number between 0 and 1
+            float randProd = (float) Math.random();
+
+            if (randProd > 0.0f && randProd <= 0.33f) {
+                word = word.replaceAll("F", rules.get(0));
+            } else if (randProd > 0.33f && randProd <= 0.66f) {
+                word = word.replaceAll("F", rules.get(1));
+            } else if (randProd > 0.66f && randProd <= 1.0f) {
+                word = word.replaceAll("F", rules.get(2));
+            } else {
+                System.out.println("Ninguno");
             }
-            
+
+            System.out.println("Add word (" + i + "): " + word);
             productions.add(word);
         }
+
     }
-    
-    
+
     public String getProduction(int i) {
         return productions.get(i);
     }
@@ -70,5 +57,5 @@ public class LSystemProduction extends Thread {
     public Vector<String> getProductions() {
         return productions;
     }
-    
+
 }
